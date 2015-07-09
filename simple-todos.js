99612388b441fcdcaae82d452398ed3ec40c12d1
Kyle Lortscher <kylelortscher@gmail.com>
2015-07-09 15:51:32 -0700

    First Commit

---
+++ simple-todos.js
@@ -0,0 +1,22 @@
+Tasks = new Mongo.Collection("tasks");
+
+if (Meteor.isClient) {
+  Template.body.helpers({
+    tasks: [
+      { text: "This is task 1" },
+      { text: "This is task 2" },
+      { text: "This is task 3" }
+    ]
+  });
+  Template.body.helpers({
+    tasks: function () {
+      return Tasks.find({});
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
