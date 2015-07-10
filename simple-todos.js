54ddea7276e7210d9b50cef54228c4d88c69051a
Kyle Lortscher <kylelortscher@gmail.com>
2015-07-09 19:27:24 -0700

    Added Security and Task Method

--- simple-todos.js
+++ simple-todos.js
@@ -26,7 +26,7 @@ if (Meteor.isClient) {
 
       var text = event.target.text.value;
 
-      Tasks.insert({
+      Meteor.call("addTask", text);({
         text: text,
         createdAt: new Date(),            // current time
         owner: Meteor.userId(),           // _id of logged in user
@@ -46,16 +46,38 @@ if (Meteor.isClient) {
   Template.task.events({
     "click .toggle-checked": function () {
       //Set The Checked Property Opposite To Its Current Value
-      Tasks.update(this._id, {$set: {checked: ! this.checked}});
+      Meteor.call("setChecked", this._id, ! this.checked);(this._id, {$set: {checked: ! this.checked}});
     },
     "click .delete": function () {
-      Tasks.remove(this._id);
+      Meteor.call("deleteTask", this._id);
     }
   });
   Accounts.ui.config({
     passwordSignupFields: "USERNAME_ONLY"
   });
 }
+
+Meteor.methods({
+  addTask: function (text) {
+    // Make sure the user is logged in before inserting a task
+    if (! Meteor.userId()) {
+      throw new Meteor.Error("not-authorized");
+    }
+
+    Tasks.insert({
+      text: text,
+      createdAt: new Date(),
+      owner: Meteor.userId(),
+      username: Meteor.user().username
+    });
+  },
+  deleteTask: function (taskId) {
+    Tasks.remove(taskId);
+  },
+  setChecked: function (taskId, setChecked) {
+    Tasks.update(taskId, { $set: { checked: setChecked} });
+  }
+});
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
