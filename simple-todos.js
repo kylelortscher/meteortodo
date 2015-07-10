56b729e6564c2802dd815e7d7f67a69ae4965fea
Kyle Lortscher <kylelortscher@gmail.com>
2015-07-09 18:12:39 -0700

    Added user authentication

--- simple-todos.js
+++ simple-todos.js
@@ -28,7 +28,9 @@ if (Meteor.isClient) {
 
       Tasks.insert({
         text: text,
-        createdAt: new Date()
+        createdAt: new Date(),            // current time
+        owner: Meteor.userId(),           // _id of logged in user
+        username: Meteor.user().username  // username of logged in user
       });
 
       event.target.text.value = "";
@@ -49,6 +51,9 @@ if (Meteor.isClient) {
     "click .delete": function () {
       Tasks.remove(this._id);
     }
+  });
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
   });
 }
 
